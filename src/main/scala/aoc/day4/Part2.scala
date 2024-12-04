68b1259b8f8dd25385fb5d8bf7a496f651dcc515
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2024-12-05 02:23:05 +0530

    day 4 refactor

--- src/main/scala/aoc/day4/Part2.scala
+++ src/main/scala/aoc/day4/Part2.scala
@@ -4,44 +4,27 @@ package day4
 object Part2 extends Challenge(day(4).part(2)):
   val width = input.head.length
   val height = input.length
-  case class Position(x: Int, y: Int):
-    def isInGrid: Boolean = x.between(0, width - 1) && y.between(0, height - 1)
-    def value: Char = input(y)(x)
-  case class Shape(
-      center: Position,
-      topLeft: Position,
-      topRight: Position,
-      bottomLeft: Position,
-      bottomRight: Position
-  ):
-    def isXmas =
-      Seq(center, topLeft, topRight, bottomLeft, bottomRight).forall(_.isInGrid) &&
-        center.value == 'A' &&
-        ((topLeft.value == 'M' && bottomRight.value == 'S') || (topLeft.value == 'S' && bottomRight.value == 'M')) &&
-        ((topRight.value == 'M' && bottomLeft.value == 'S') || (topRight.value == 'S' && bottomLeft.value == 'M'))
 
   def execute: Long =
     (for
       x <- 0 until width
       y <- 0 until height
-      s = shape(Position(x, y))
-      if s.isXmas
-    yield s).length
+      if Shape(Position(x, y)).isXmas
+    yield ()).length
 
-  def shape(center: Position): Shape =
-    enum Dir:
-      case Right, Left, Top, Bottom
+  case class Position(x: Int, y: Int):
+    def isInGrid: Boolean = x.between(0, width - 1) && y.between(0, height - 1)
+    def value: Char = input(y)(x)
+  case class Line(seq: Seq[Position]):
+    override def toString(): String = seq.map(_.value).mkString
+  case class Shape(cells: Seq[Position]):
+    def isXmas =
+      val isValid = cells.forall(_.isInGrid)
+      val line1 = Line(Seq(cells(1), cells(0), cells(4)))
+      val line2 = Line(Seq(cells(2), cells(0), cells(3)))
+      isValid && (line1.toString == "MAS" || line1.toString == "SAM") && (line2.toString == "MAS" || line2.toString == "SAM")
 
-    def select(y: Dir, x: Dir): Position =
-      val xVal = x match
-        case Dir.Right => 1
-        case Dir.Left  => -1
-        case _         => 0
-      val yVal = y match
-        case Dir.Top    => -1
-        case Dir.Bottom => 1
-        case _          => 0
-      Position(center.x + xVal, center.y + yVal)
-
-    import Dir.*
-    Shape(center, select(Top, Left), select(Top, Right), select(Bottom, Left), select(Bottom, Right))
+  object Shape:
+    def apply(p: Position): Shape =
+      def move(x: Int, y: Int): Position = Position(p.x + x, p.y + y)
+      Shape(Seq(p, move(-1, -1), move(-1, 1), move(1, -1), move(1, 1)))
